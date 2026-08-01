a1244ad03ada206a14e4e767062f7b1e8da4c2b2
Aaditya <555aaditya@proton.me>
2026-08-01 16:27:13 +0530

    feat: add corpus download and content-hashed document-level train/val split

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -10,6 +10,16 @@ from dataset.binfile import (
     read_tokens_bin,
     write_tokens_bin,
 )
+from dataset.corpora import (
+    CORPORA,
+    Corpus,
+    download,
+    prepare,
+    prepare_documents,
+    read_documents,
+    split_documents,
+    use_system_trust_store,
+)
 from dataset.engine import DataEngine
 from dataset.token_dataset import (
     MemmapTokenDataset,
@@ -18,13 +28,21 @@ from dataset.token_dataset import (
 )
 
 __all__ = [
+    "CORPORA",
     "TOKEN_DTYPE",
+    "Corpus",
     "DataEngine",
     "MemmapTokenDataset",
     "StreamingTokenDataset",
     "SyntheticTokenDataset",
     "count_tokens_bin",
+    "download",
     "encode_texts_to_bin",
+    "prepare",
+    "prepare_documents",
+    "read_documents",
     "read_tokens_bin",
+    "split_documents",
+    "use_system_trust_store",
     "write_tokens_bin",
 ]
